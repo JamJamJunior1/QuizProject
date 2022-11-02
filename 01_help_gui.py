7ed3f23ae74c84fd774a6a14ea650c540109be91
JamJamJunior1 <jn18021@melville-high.school.nz>
2022-11-03 11:45:31 +1300

    I believe this is finished off now but to be honest, it might need tweaking later on, but for now, its perfect the way it is, now the hard part which is making a q&a interface with recycled code

--- 01_help_gui.py
+++ 01_help_gui.py
@@ -17,14 +17,14 @@ class Quiz:
 
         #Temperature Conversion Heading (row 0)
         self.quiz_label = Label(self.quiz_frame, text="Cool Quiz Boogaloo",
-                                          font=("Ubuntu", "16", "bold"),
+                                          font=("Arial", "14", "bold"),
                                           bg=background_color,
                                           padx=10, pady=10)
         self.quiz_label.grid(row=0)
 
         # Help Button (row 1)
         self.help_button = Button(self.quiz_frame, text="Help",
-                                  font="ubuntu 14", padx=10, pady=10, command=self.help)
+                                  font="Arial 14", padx=10, pady=10, command=self.help)
         self.help_button.grid(row=1)
 
     def help(self):
@@ -35,7 +35,7 @@ class Quiz:
 class Help:
     def __init__(self, partner):
 
-        background = "red"
+        background = "maroon"
 
         # disable help button
         partner.help_button.config(state=DISABLED)
@@ -52,17 +52,17 @@ class Help:
 
         # Set up Help heading (row 0)
         self.how_heading = Label(self.help_frame, text="Help / Instructions",
-                                 font="ubuntu 10 bold", bg=background)
+                                 font="Arial 14 bold", bg=background)
         self.how_heading.grid(row=0)
 
         # Help text (label, row 1)
-        self.help_text = Label(self.help_frame, text="",
+        self.help_text = Label(self.help_frame, text="hi",
                                justify=LEFT, width=40, bg=background, wrap=250)
         self.help_text.grid(row=1)
 
         # Dismiss button (row 2)
         self.dismiss_btn = Button(self.help_frame, text="Dismiss", width=10, bg="orange",
-                                  font="ubuntu 10 bold", command=partial(self.close_help, partner))
+                                  font="Arial 14 bold", command=partial(self.close_help, partner))
         self.dismiss_btn.grid(row=2, pady=10)
 
     def close_help(self, partner):
